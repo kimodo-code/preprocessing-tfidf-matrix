b68d6b1ccba8775ca2549f1f810c11d63b41be0f
kimodo <50057356+kimodo-code@users.noreply.github.com>
2022-01-15 19:56:06 +0000

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -5,9 +5,6 @@ public class Driver {//run
 
 	public static void main (String [] args) throws IOException {
 
-
-
-
 		//TASK1
 		Task1 t1 = new Task1();//create task 1 object
 		t1.goTask1();//task 1 object and task1 method incorporating all the task 1 instructions => execute
@@ -20,7 +17,6 @@ public class Driver {//run
 		Task3 t3 = new Task3();//create task 3 object
 		t3.createTFIDFMatrix("output_t3.txt");//TYPE IN FILENAME HERE
 
-
 		//TASK4
 		Task4 t4 = new Task4();
 		double [] documentA =  t4.getArrayA();//fetch array a from the first method
@@ -29,14 +25,8 @@ public class Driver {//run
 		double [] documentB = t4.getArrayB();//fetch array b from the second method
 		//System.out.println(Arrays.toString(documentB));//check
 
-
-
 		double answer = t4.cosineSimilarity(documentA, documentB);//pass the two arrays into the cosine similarity method
 		System.out.println("\n" + answer);//check answer - single figure ok
 		t4.printAnswer(answer);//PRINT THE ANSWER TO CONSOLE
-
-
-
 	}
-
 }
